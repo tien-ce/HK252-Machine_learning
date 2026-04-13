469c7214fa0055f9dd0bb60c0d709a1b5a1a86f2
Bao <hgbao26@gmail.com>
2026-04-13 16:52:34 +0700

    update

--- model/model.py
+++ model/model.py
@@ -12,7 +12,7 @@ def choose_model(command, random_state= 42):
     elif command == 'Random_Forest_Library_Model':
         model = Random_Forest_Model(random_state= random_state)
     elif command == 'Random_Forest_Model':
-        model = RandomForestClassifier(n_estimators = 1, feature_subset_size = 4, max_depth = 5, min_samples_split = 3, random_state= random_state)
+        model = RandomForestClassifier(n_estimators = 10, feature_subset_size = 4, max_depth = 5, min_samples_split = 3, random_state= random_state)
     else:
         model = SVM_Model(random_state= random_state)
 
